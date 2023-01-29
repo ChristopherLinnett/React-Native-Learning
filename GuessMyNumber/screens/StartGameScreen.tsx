13000b58df208c9193e5e010e9f1b1119dd7d1c8
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-01-29 20:53:35 +1000

    GuessMyNumber: Clear Default Code add page files with named empty components

---
+++ GuessMyNumber/screens/StartGameScreen.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { View } from "react-native/types";
+
+const StartGameScreen = (props:any) => { 
+    return (<View></View>);
+}
+
+export default StartGameScreen;
