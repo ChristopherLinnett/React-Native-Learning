c900bc4511012a3661859a0934bc26e45aac9756
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-01-29 22:23:51 +1000

    gave primary button component styling including - radius, background colour, padding, shadowing, text colour, textalign - pressed styles including ripple android and opacity for both

--- GuessMyNumber/components/PrimaryButton.tsx
+++ GuessMyNumber/components/PrimaryButton.tsx
@@ -1,10 +1,47 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React from 'react';
+import {View, Text, Pressable, StyleSheet} from 'react-native';
 
-const PrimaryButton = (props: {children?:String}) => { 
-    return (<View>
-        <Text>{props.children}</Text>
-    </View>);
-}
+const PrimaryButton = (props: {children?: String}) => {
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({pressed}) =>
+          pressed
+            ? [styles.pressed, styles.buttonInnerContainer]
+            : styles.buttonInnerContainer
+        }
+        android_ripple={{color: '#640233'}}
+        onPress={() => {
+          console.log('pressed');
+        }}>
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+  },
+  buttonInnerContainer: {
+    backgroundColor: '#4e0329',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: {width: 4, height: 4},
+    shadowOpacity: 0.75,
+    shadowRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
 
 export default PrimaryButton;
