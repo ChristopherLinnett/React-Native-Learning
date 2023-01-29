77c3dcdbf7a42beef305a739407f400434200ccd
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-01-29 23:05:49 +1000

    add background image to project add gradient to main app overlay gradient with low opacity background image

--- GuessMyNumber/App.tsx
+++ GuessMyNumber/App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
-import {
-  StyleSheet, View,
-} from 'react-native';
+import {StyleSheet, ImageBackground} from 'react-native';
 import StartGameScreen from './screens/StartGameScreen';
+import LinearGradient from 'react-native-linear-gradient';
 
 export default function App(): JSX.Element {
   return (
-    <View style={styles.background}>
-    <StartGameScreen />
-    </View>
+    <LinearGradient colors={['#72063c', '#ddb52f']} style={styles.rootScreen}>
+      <ImageBackground
+        style={styles.rootScreen}
+        resizeMode="cover"
+        source={require('./assets/images/background.png')}
+        imageStyle={styles.backgroundImage}>
+        <StartGameScreen />
+      </ImageBackground>
+    </LinearGradient>
   );
 }
 const styles = StyleSheet.create({
-  background: {
-    backgroundColor: 'white',
-    flex: 1
-  }
-  });
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
