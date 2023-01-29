0557e0ffdec90210c0bbd0ca86d840d75172fa16
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-01-29 22:39:13 +1000

    aligned buttons in even sized row with views/styles

--- GuessMyNumber/screens/StartGameScreen.tsx
+++ GuessMyNumber/screens/StartGameScreen.tsx
@@ -1,39 +1,59 @@
-import React from "react";
-import { View, TextInput, StyleSheet } from "react-native";
-import PrimaryButton from "../components/PrimaryButton";
+import React from 'react';
+import {View, TextInput, StyleSheet} from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
 
-const StartGameScreen = (props:any) => { 
-    return (<View style={style.textInputContainer}>
-        <TextInput style={style.numberInput} maxLength={2} keyboardType='number-pad' />
-        <PrimaryButton>Reset</PrimaryButton>
-        <PrimaryButton>Confirm</PrimaryButton>
-    </View>);
-}
+const StartGameScreen = (props: any) => {
+  return (
+    <View style={style.textInputContainer}>
+      <TextInput
+        style={style.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
+      <View style={style.buttonsContainer}>
+        <View style={style.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={style.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
+};
 
 const style = StyleSheet.create({
-    textInputContainer: {
-        padding: 16,
-        marginHorizontal: 24,
-        marginTop: 100,
-        borderRadius: 8,
-        backgroundColor: '#72063c',
-        elevation: 4,
-        shadowColor: 'black',
-        shadowOffset: {width: 4, height: 4},
-        shadowRadius: 6,
-        shadowOpacity: .75,
-    },
-    numberInput: {
-        height: 50,
-        width: 50,
-        fontSize: 32,
-        borderBottomColor:'#ddb52f',
-        borderBottomWidth: 2,
-        color: '#ddb52f',
-        marginVertical: 8,
-        fontWeight: 'bold',
-        textAlign: 'center',
-    }
-})
+  textInputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    marginHorizontal: 24,
+    marginTop: 100,
+    borderRadius: 8,
+    backgroundColor: '#72063c',
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOffset: {width: 4, height: 4},
+    shadowRadius: 6,
+    shadowOpacity: 0.75,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  buttonsContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {flex: 1},
+});
 
 export default StartGameScreen;
