44e5fcb7901c91d13d9e169b910bd591404b74fa
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-01-29 21:38:23 +1000

    added styling to input container with shadowing gave styling to textinput element set a maxelength and keyboard type

--- GuessMyNumber/screens/StartGameScreen.tsx
+++ GuessMyNumber/screens/StartGameScreen.tsx
@@ -3,16 +3,36 @@ import { View, TextInput, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 const StartGameScreen = (props:any) => { 
-    return (<View style={style.startGameScreen}>
-        <TextInput />
+    return (<View style={style.textInputContainer}>
+        <TextInput style={style.numberInput} maxLength={2} keyboardType='number-pad' />
         <PrimaryButton>Reset</PrimaryButton>
         <PrimaryButton>Confirm</PrimaryButton>
     </View>);
 }
 
 const style = StyleSheet.create({
-    startGameScreen: {
-        flex: 1,
+    textInputContainer: {
+        padding: 16,
+        marginHorizontal: 24,
+        marginTop: 100,
+        borderRadius: 8,
+        backgroundColor: '#72063c',
+        elevation: 4,
+        shadowColor: 'black',
+        shadowOffset: {width: 4, height: 4},
+        shadowRadius: 6,
+        shadowOpacity: .75,
+    },
+    numberInput: {
+        height: 50,
+        width: 50,
+        fontSize: 32,
+        borderBottomColor:'#ddb52f',
+        borderBottomWidth: 2,
+        color: '#ddb52f',
+        marginVertical: 8,
+        fontWeight: 'bold',
+        textAlign: 'center',
     }
 })
 
