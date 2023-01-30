7b3e5503bfb7a9754a5f0f330393afef37eac9a7
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-01-31 00:02:44 +1000

    built gameoverscreen ui added image for gameover screen

--- GuessMyNumber/screens/GameOverScreen.tsx
+++ GuessMyNumber/screens/GameOverScreen.tsx
@@ -1,8 +1,69 @@
 import React from 'react';
-import {View} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
+import {Image, View} from 'react-native';
+import PrimaryButton from '../components/UI/PrimaryButton';
+import Title from '../components/UI/title';
+import Colours from '../constants/colours';
 
 const GameOverScreen = () => {
-  return <View />;
+  return (
+    <View style={styles.root}>
+      <Title>GAME OVER</Title>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/success.png')}
+        />
+      </View>
+      <Text style={styles.centerText}>
+        Your Phone Needed <Text style={styles.highlightText}>X</Text> Attempts
+        to Guess Your Number <Text style={styles.highlightText}>Y</Text>
+      </Text>
+      <View style={styles.buttonTopContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton onPressed={() => {}}>New Game</PrimaryButton>
+        </View>
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    padding: 24,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageContainer: {
+    borderRadius: 150,
+    width: 300,
+    height: 300,
+    borderWidth: 3,
+    borderColor: Colours.primary800,
+    overflow: 'hidden',
+    margin: 36,
+  },
+  image: {
+    width: '100%',
+    height: '100%',
+  },
+  centerText: {
+    textAlign: 'center',
+    fontSize: 24,
+    marginVertical: 24,
+  },
+  buttonTopContainer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    flex: 1,
+    marginBottom: 24,
+  },
+  highlightText: {
+    color: Colours.primary500,
+    fontWeight: 'bold',
+  },
+});
 
 export default GameOverScreen;
