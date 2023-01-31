29d9b72ba2eaf6aa60fcf36d059bd6cdf6244c2c
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-01-31 11:43:02 +1000

    create blank new project MealsApp Move boilerplate to my preferred style

---
+++ mealsapp/lib/App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+function App(): JSX.Element {
+  return <View style={styles.root} />;
+}
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
+
+export default App;
