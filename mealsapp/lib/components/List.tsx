ec67566cf02dcb937322f6a212de7017954b13d0
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-02-01 19:08:29 +1000

    create list component for displaying list strings

---
+++ mealsapp/lib/components/List.tsx
@@ -0,0 +1,37 @@
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+
+type ListItemProps = {data: string[]};
+
+const List = ({data}: ListItemProps) => {
+  const list = data?.map(dataPoint => (
+    <View style={styles.container}>
+      <Text key={dataPoint} style={styles.textStyle}>
+        {dataPoint}
+      </Text>
+    </View>
+  ));
+
+  return <View style={styles.centered}>{list}</View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    padding: 10,
+    marginVertical: '2%',
+    width: '80%',
+    backgroundColor: '#505A5B',
+    borderRadius: 20,
+  },
+  textStyle: {
+    fontSize: 14,
+    color: 'white',
+  },
+  centered: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
+export default List;
