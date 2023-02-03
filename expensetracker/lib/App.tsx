eed13c54bb701ca8b44fa2d4f27a60c115e4f702
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-02-03 13:23:24 +1000

    create app specific theme file do basic colouring of main page

--- expensetracker/lib/App.tsx
+++ expensetracker/lib/App.tsx
@@ -3,6 +3,8 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import {StatusBar} from 'react-native';
+import colours from './constants/colours';
+import {GlobalTheme} from './constants/theme';
 import AllExpenses from './pages/AllExpenses';
 import EditExpense from './pages/EditExpense';
 import RecentExpenses from './pages/RecentExpenses';
@@ -12,8 +14,22 @@ const Stack = createNativeStackNavigator();
 
 const BottomTabsComponent = () => {
   return (
-    <BottomTabs.Navigator>
-      <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+    <BottomTabs.Navigator
+      screenOptions={{
+        headerStyle: {backgroundColor: GlobalTheme.colors.primary800},
+        headerTintColor: colours.white,
+        tabBarStyle: {backgroundColor: GlobalTheme.colors.primary800},
+        tabBarActiveTintColor: colours.white,
+        tabBarInactiveTintColor: GlobalTheme.colors.primary400,
+      }}>
+      <BottomTabs.Screen
+        name="RecentExpenses"
+        options={{
+          title: 'Recent Expenses',
+          tabBarLabel: 'Recent',
+        }}
+        component={RecentExpenses}
+      />
       <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
     </BottomTabs.Navigator>
   );
---
+++ expensetracker/lib/constants/theme.tsx
@@ -0,0 +1,15 @@
+import colours from './colours';
+
+export const GlobalTheme = {
+  colors: {
+    primary50: colours.honey_dew,
+    primary100: colours.chestnut_rose,
+    primary200: colours.thunderbird,
+    primary400: colours.chili_pepper,
+    primary500: colours.fireEngineRed,
+    primary700: colours.scarlet,
+    primary800: colours.maroon,
+    accent500: colours.navy,
+    error50: colours.blue_grey,
+  },
+};
