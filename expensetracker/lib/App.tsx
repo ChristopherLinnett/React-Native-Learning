a34a0300a4a9a3fb26c3707d7b94320cfda64574
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-02-03 12:58:36 +1000

    set up navigators on app screen demonstrates stack and tabs

--- expensetracker/lib/App.tsx
+++ expensetracker/lib/App.tsx
@@ -1,12 +1,39 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
-import {View} from 'react-native';
+import {StatusBar} from 'react-native';
+import AllExpenses from './pages/AllExpenses';
+import EditExpense from './pages/EditExpense';
+import RecentExpenses from './pages/RecentExpenses';
+
+const BottomTabs = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const BottomTabsComponent = () => {
+  return (
+    <BottomTabs.Navigator>
+      <BottomTabs.Screen name="RecentExpenses" component={RecentExpenses} />
+      <BottomTabs.Screen name="AllExpenses" component={AllExpenses} />
+    </BottomTabs.Navigator>
+  );
+};
 
 function App(): JSX.Element {
   return (
-    <NavigationContainer>
-      <View />
-    </NavigationContainer>
+    <>
+      <StatusBar barStyle="default" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="BottomTabs"
+            component={BottomTabsComponent}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen name="EditExpense" component={EditExpense} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
 
