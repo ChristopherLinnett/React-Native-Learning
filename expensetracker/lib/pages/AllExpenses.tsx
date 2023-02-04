bcddde426ec47e403b4a2d2fed08f1023cdf7605
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-02-04 17:22:50 +1000

    pages use expense

--- expensetracker/lib/pages/AllExpenses.tsx
+++ expensetracker/lib/pages/AllExpenses.tsx
@@ -1,21 +1,13 @@
-import React from 'react';
-import Expense from '../models/expense';
+import React, {useContext} from 'react';
+import {ExpensesContext} from '../store/expenses.context';
 import ExpensesOutput from '../widgets/ExpenseOutput/ExpensesOutput';
 
-const DUMMY_EXPENSES = [
-  new Expense('e1', 'A pair of Shoes', 59.99, new Date('2023-1-23')),
-  new Expense('e2', 'A pair of shorts', 19.99, new Date('2023-2-2')),
-  new Expense('e3', 'A shirt', 23.5, new Date('2023-1-1')),
-  new Expense(
-    'e4',
-    'Replacement Catalytic Converter',
-    800,
-    new Date('2023-2-2'),
-  ),
-];
-
 const AllExpenses = () => {
-  return <ExpensesOutput timePeriod="All Expenses" expenses={DUMMY_EXPENSES} />;
+  const expenseCtx = useContext(ExpensesContext);
+
+  return (
+    <ExpensesOutput timePeriod="All Expenses" expenses={expenseCtx.expenses} />
+  );
 };
 
 export default AllExpenses;
--- expensetracker/lib/pages/RecentExpenses.tsx
+++ expensetracker/lib/pages/RecentExpenses.tsx
@@ -1,21 +1,17 @@
-import React from 'react';
-import Expense from '../models/expense';
+import React, {useContext} from 'react';
+import {ExpensesContext} from '../store/expenses.context';
+import getDateMinusDays from '../util/date';
 import ExpensesOutput from '../widgets/ExpenseOutput/ExpensesOutput';
 
-const DUMMY_EXPENSES = [
-  new Expense('e1', 'A pair of Shoes', 59.99, new Date('2023-1-23')),
-  new Expense('e2', 'A pair of shorts', 19.99, new Date('2023-2-2')),
-  new Expense('e3', 'A shirt', 23.5, new Date('2023-1-1')),
-  new Expense(
-    'e4',
-    'Replacement Catalytic Converter',
-    800,
-    new Date('2023-2-2'),
-  ),
-];
-
 const RecentExpenses = () => {
-  return <ExpensesOutput timePeriod="Last 7 Days" expenses={DUMMY_EXPENSES} />;
+  const expensesContext = useContext(ExpensesContext);
+  const recentExpenses = expensesContext.expenses.filter(expense => {
+    const today = new Date();
+    const date7daysago = getDateMinusDays(today, 7);
+    return expense.date > date7daysago;
+  });
+
+  return <ExpensesOutput timePeriod="Last 7 Days" expenses={recentExpenses} />;
 };
 
 export default RecentExpenses;
