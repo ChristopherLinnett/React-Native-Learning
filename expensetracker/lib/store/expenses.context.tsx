1dff381ba700628c67ec4acd9d8fc97acb495c8c
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-02-04 17:16:08 +1000

    create a context for managing expense create a date helper function

---
+++ expensetracker/lib/store/expenses.context.tsx
@@ -0,0 +1,104 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {createContext, ReactNode, useReducer} from 'react';
+import Expense from '../models/expense';
+import DUMMY_EXPENSES from '../constants/DummyData';
+type ExpenseContextProps = {
+  expenses: Expense[];
+  addExpense: Function;
+  updateExpense: Function;
+  deleteExpense: Function;
+};
+
+export const ExpensesContext = createContext<ExpenseContextProps>({
+  expenses: [],
+  addExpense: (expense: Expense) => {},
+  updateExpense: (id: string, newExpense: Expense) => {},
+  deleteExpense: (id: string) => {},
+});
+
+const expensesReducer = (
+  state: Expense[],
+  action: {
+    type: 'ADD' | 'UPDATE' | 'DELETE';
+    payload: {id?: string | undefined; expense?: Expense | undefined};
+  },
+) => {
+  switch (action.type) {
+    case 'ADD':
+      if (
+        action.payload?.id === undefined ||
+        action.payload?.expense === undefined
+      ) {
+        return state;
+      }
+      const expense = action.payload.expense;
+      return [
+        new Expense(
+          Math.random().toString(),
+          expense.description,
+          expense.amount,
+          expense.date,
+        ),
+        ...state,
+      ];
+
+    case 'UPDATE':
+      if (
+        action.payload?.id === undefined ||
+        action.payload?.expense === undefined
+      ) {
+        return state;
+      }
+      const newExpense = action.payload.expense;
+      const expenseItemsIndex = state.findIndex(
+        expenseItem => expenseItem.id === action.payload.id,
+      );
+      if (expenseItemsIndex < 0) {
+        return state;
+      }
+      state[expenseItemsIndex] = new Expense(
+        newExpense.id,
+        newExpense.description,
+        newExpense.amount,
+        newExpense.date,
+      );
+      return state;
+    case 'DELETE':
+      if (action.payload.id === undefined) {
+        return state;
+      }
+      return state.filter(eachItem => eachItem.id !== action.payload.id);
+    default:
+      return state;
+  }
+};
+const ExpensesContextProvider = ({children}: {children: ReactNode}) => {
+  const [expensesState, dispatch] = useReducer(
+    expensesReducer,
+    DUMMY_EXPENSES,
+    undefined,
+  );
+
+  const addExpense = (expense: Expense) =>
+    dispatch({type: 'ADD', payload: {expense: expense}});
+
+  const deleteExpense = (id: string) =>
+    dispatch({type: 'DELETE', payload: {id: id}});
+
+  const updateExpense = (id: string, expense: Expense) =>
+    dispatch({type: 'UPDATE', payload: {id: id, expense: expense}});
+
+  const value = {
+    expenses: expensesState,
+    addExpense: addExpense,
+    deleteExpense: deleteExpense,
+    updateExpense: updateExpense,
+  };
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
+};
+
+export default ExpensesContextProvider;
