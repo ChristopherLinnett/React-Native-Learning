9643bd109e494b3df7af549e6223db649c001702
ChristopherLinnett <chrislinnett45389@gmail.com>
2023-02-07 13:35:02 +1000

    create empty project greatplaces

---
+++ greatplaces/lib/App.tsx
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {View, SafeAreaView, StatusBar} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <StatusBar barStyle={'light-content'} />
+
+      <View />
+    </SafeAreaView>
+  );
+}
+
+export default App;
